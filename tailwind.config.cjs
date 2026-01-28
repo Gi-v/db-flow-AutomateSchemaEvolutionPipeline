259e7d79bd043dab2f632b3ac78579d953c9a3b5
Haru <143938115+Gi-v@users.noreply.github.com>
2026-01-28 21:13:55 +0000

    .

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,8 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{ts,tsx,js,jsx,html}'
+  ],
   theme: {
     extend: {}
   },
